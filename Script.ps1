487042fbb67965f11b6bac17e0b2c731519b751b
Abhiraj <abhirajsardar2003@gmail.com>
2024-02-24 19:14:32 +0530

    first automation

---
+++ Script.ps1
@@ -0,0 +1,6 @@
+﻿cd\ #go to root directory
+cd Users\"ABHIRAJ SARDAR"\Desktop #change to Desktop directory
+New-Item Greetings -ItemType Directory #creating a Directory named Greetings
+cd Greetings
+echo "Good Morning Abhiraj Sardar" > Greet.txt 
+notepad Greet.txt
