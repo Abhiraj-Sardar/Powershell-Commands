66cf045558fa4dc2ab5d8e781fdc3ffceba364a1
Abhiraj <abhirajsardar2003@gmail.com>
2024-02-24 20:17:58 +0530

    update calculator

---
+++ Calculator.ps1
@@ -0,0 +1,39 @@
+﻿#--------------Calculator Code------
+
+[int]$num1=Read-Host -Prompt "Enter The 1st Number" #used to read input from console
+[char]$op=Read-Host -Prompt "Enter The Operation[+,-,/,%,*]"
+[int]$num2=Read-Host -Prompt "Enter The 2nd Number"
+
+switch($op)
+{
+    '+'{
+        $feed="You choose Addition"
+        [int]$ans=$num1 + $num2
+        break;
+    }
+    '-'{
+        $feed="You choose Substraction"
+        [int]$ans=$num1 - $num2
+        break;
+    }
+    '*'{
+        $feed="You choose Multiplication"
+        [int]$ans=$num1 * $num2
+        break;
+    }
+   '/'{
+        $feed="You choose Division"
+        [double]$ans=$num1 / $num2
+        break;
+    }
+    '%'{
+        $feed="You choose Modulus"
+        [int]$ans=$num1 % $num2
+        break;
+    }
+
+}
+
+Write-Host("Option : $feed ") #this is used to print on the console
+Write-Host("your ans is : $ans")
+pause #this will stop the console
---
+++ DataTypes.ps1
@@ -0,0 +1,10 @@
+﻿#--------------Data Types in PowerShell--------
+$a=10
+$b=22.22
+[char] $c='A'
+$d="Abhiraj"
+
+$a.GetType().Name
+$b.GetType().Name
+$c.GetType().Name
+$d.GetType().Name
