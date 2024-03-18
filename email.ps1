c6608887a0b6247f0d7b25203c22a769fb8e00a7
Abhiraj <abhirajsardar2003@gmail.com>
2024-03-18 17:26:34 +0530

    update email.ps1

---
+++ email.ps1
@@ -0,0 +1,30 @@
+﻿
+#Don't Run This
+
+# Specify the sender and recipient email addresses
+$from = "abhirajsardar2003@gmail.com"
+$to = "abhiraj123sardar@gmail.com"
+
+# Create a MailMessage object
+$mailMessage = New-Object System.Net.Mail.MailMessage($from, $to)
+
+# Set the email subject and body
+$mailMessage.Subject = "Test Email"
+$mailMessage.Body = "This is a test email sent using PowerShell automation."
+
+# Specify the SMTP server details
+$smtpServer = "smtp.google.com"
+$smtpPort = 465
+
+# Create a SmtpClient object
+$smtpClient = New-Object System.Net.Mail.SmtpClient($smtpServer, $smtpPort)
+#$smtpClient.EnableSsl = $true
+
+# Specify the credentials for authentication (replace with your actual credentials)
+$smtpClient.Credentials = New-Object System.Net.NetworkCredential("abhirajsardar2003@gmail.com", "AbHi2003")
+
+# Send the email
+$smtpClient.Send($mailMessage)
+
+# Output a message indicating that the email has been sent
+Write-Host "Email sent successfully."
